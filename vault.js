16e5d27287418deb402f8f4275fd1a87557e7d9b
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-01-06 15:30:25 -1000

    cleanup

--- vault.js
+++ vault.js
@@ -3,21 +3,21 @@ module.exports = function() {
 
     let _theBox = {};
 
-  var setValue = function(key, value){
-    _theBox[key] = value;
-  }
-
-  var getValue = function(key){
-    if(_theBox[key]){
-        return _theBox[key];
-    } else{
-        return null;
+    var setValue = function(key, value){
+        _theBox[key] = value;
     }
-  }
 
-  const myVault = {
-      setValue:setValue,
-      getValue:getValue
-  }
+    var getValue = function(key){
+        if(_theBox[key]){
+            return _theBox[key];
+        } else{
+            return null;
+        }
+    }
+
+    const myVault = {
+        setValue:setValue,
+        getValue:getValue
+    }
   return myVault;
 };
