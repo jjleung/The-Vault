237eb278aecbeefa88476545ee62fafc3465f2fa
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-01-06 15:25:38 -1000

    all pass

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
-  
+
+    let _theBox = {};
+
+  var setValue = function(key, value){
+    _theBox[key] = value;
+  }
+
+  var getValue = function(key){
+    if(_theBox[key]){
+        return _theBox[key];
+    } else{
+        return null;
+    }
+  }
+
+  const myVault = {
+      setValue:setValue,
+      getValue:getValue
+  }
+  return myVault;
 };
